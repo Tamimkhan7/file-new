81c70f8ad53433c5c546be4eea95347835b705e7
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2022-11-18 00:11:21 +0600

    first commit

---
+++ exp functton use of c++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x, result;
+    cin>>x;
+    result = exp(x);
+    cout << "exp(x) = " << result << endl;
+
+    return 0;
+}
